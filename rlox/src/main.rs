cea5cbc8c22ad04e11545f8126ba0841ad1cf12d
hachibu <raymondsohn@gmail.com>
2018-10-01 01:24:22 +0900

    Add basic REPL
    Add basic REPL.

--- rlox/src/main.rs
+++ rlox/src/main.rs
@@ -1,16 +1,30 @@
 mod syntax;
 
+use std::io::{self, Write};
 use syntax::Scanner;
 
-fn main() {
-    let mut scanner = Scanner::new("
-        var x = 1;
-        var y = $;
-    ");
-    scanner.source_file = Some("src/main.lox");
+fn main() -> io::Result<()> {
+    loop {
+        io::stdout().write(b">>> ")?;
+        io::stdout().flush()?;
 
-    match scanner.scan_tokens() {
-        Ok(tokens) => println!("{:?}", tokens),
-        Err(error) => println!("{}", error)
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let mut scanner = Scanner::new(&input);
+
+                scanner.source_file = Some("stdin");
+
+                match scanner.scan_tokens() {
+                    Ok(tokens) => {
+                        for token in tokens {
+                            println!("{:?}", token)
+                        }
+                    },
+                    Err(error) => println!("{}", error)
+                }
+            }
+            Err(error) => println!("{}", error),
+        }
     }
 }
